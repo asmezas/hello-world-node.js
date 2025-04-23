0b00920816dd58460295690759ddbea979f6318f
“Mantas <mantas.samuolis@devoteam.com>
2025-04-23 16:57:00 +0300

    a

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const cors = require('cors')
 const crypto = require('crypto');
 const pkg = require('./package.json');
 
-
 // App constants
 const port = process.env.PORT || 3000;
 const apiPrefix = '/api';
